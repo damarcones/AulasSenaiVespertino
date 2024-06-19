657f6279d5281e0dd35085b26812355ba319dc5f
Damarcones Porto <damarcones@gmail.com>
2024-06-19 14:19:02 -0300

    updade

--- estoque/src/controle/Pao.java
+++ estoque/src/controle/Pao.java
@@ -2,6 +2,8 @@ package src.controle;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class Pao extends Produto {
     private String tipoPao;
@@ -14,14 +16,36 @@ public class Pao extends Produto {
         this.tipoPao = tipoPao;
     }
 
-    @Override
-    @SuppressWarnings("deprecation")
-    public String validade(Date fabricacao){
-        SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
+    public LocalDate validade(LocalDate fabricacao){
+        // LSimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
   
-        fabricacao.setDate(fabricacao.getDate() + 1);
-
-        return fs.format(fabricacao);
+        fabricacao.plusDays(10);
+        
+        return fabricacao;
 
     }
+
+    public String validade(LocalDate fabricacao, LocalDate validade){
+        // LSimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
+            LocalDate dataAtual = LocalDate.now(); 
+        if(validade.isBefore(dataAtual))
+            return "Produto Vencido";
+        else if( fabricacao.isBefore(dataAtual))
+            return "Dentro da validade";
+        
+            else 
+                return "Datas Inválidas";
+                
+            }
+
+    
+    public long compara(LocalDate fabricacao, LocalDate validade){
+        long valor = ChronoUnit.DAYS.between(fabricacao, validade);
+
+        // Period diferenca = Period.between(fabricacao, validade);
+        // int valor = Math.abs(diferenca.getDays());
+
+        return valor;
+    }
 }
+
