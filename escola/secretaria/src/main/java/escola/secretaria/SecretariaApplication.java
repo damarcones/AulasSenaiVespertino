71eab81d28ff1154ec2f5302ee9eca7b46f96d4d
Damarcones Porto <damarcones@gmail.com>
2024-07-12 17:26:01 -0300

    Subindo estrutura base

---
+++ escola/secretaria/src/main/java/escola/secretaria/SecretariaApplication.java
@@ -0,0 +1,13 @@
+package escola.secretaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariaApplication.class, args);
+	}
+
+}
---
+++ escola/secretaria/src/test/java/escola/secretaria/SecretariaApplicationTests.java
@@ -0,0 +1,13 @@
+package escola.secretaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
