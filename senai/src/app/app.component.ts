d1d310ba1ccd7c015fc5dfe2154ce931d1d5e998
Damarcones Porto <damarcones@gmail.com>
2024-08-08 18:00:59 -0300

    formulario simples e inicio de calculadora

--- senai/src/app/app.component.ts
+++ senai/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(){
+  }
+  ngOnInit(): void {
+      
+  }
   title = 'senai';
+
+  displayMessage(msg: String) { 
+    alert('O carro com a placa ' + msg + ' foi cadastrado!'); }
+
+
+  soma(a: String, b: String){
+    let numA = Number(a);
+    let numB = Number(b);
+    return numA + numB;
+  }
 }
