55eea73f3dae5c4eebc86d7801a51b023da9bed9
Yron Matheus <lightningmadxtch@gmail.com>
2024-08-14 14:15:02 -0300

    Add files via upload

---
+++ YronTrabalhoSpringBoot/yrontrabalhospringboot/src/main/java/com/yrontrabalhospringboot/Repository/VeiculoRepository.java
@@ -0,0 +1,12 @@
+package com.yrontrabalhospringboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yrontrabalhospringboot.model.VeiculoModel;
+
+
+
+
+public interface VeiculoRepository extends JpaRepository<VeiculoModel, String>{
+    
+}
